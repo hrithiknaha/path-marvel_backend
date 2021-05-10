19344074bf8e01d419149d117ea20445791c489c
Hrithik Naha <naha.hrithkk@gmail.com>
2021-05-11 02:15:06 +0530

    add caching

--- routes/FeedDetails.js
+++ routes/FeedDetails.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const axios = require("axios");
 
 const md5 = require("blueimp-md5");
-const { json } = require("express");
 const publickey = "a9985b299f04decc1427fb3b8d140dd6";
 const privatekey = "f74a2c2d21ce4f90978e52bcd6d3108250293f1a";
 const ts = new Date().getTime();
@@ -11,7 +10,44 @@ const hash = md5(stringToHash);
 
 const client = require("../utils/redis");
 
-router.get("/characters/:id", (req, res) => {
+const cacheCharacters = (req, res, next) => {
+	client.get(req.params.id, (err, data) => {
+		if (err) throw err;
+
+		if (data !== null) {
+			console.log(JSON.parse(data));
+			return res.json(JSON.parse(data));
+		} else {
+			next();
+		}
+	});
+};
+
+const cacheComics = (req, res, next) => {
+	client.get(req.params.id, (err, data) => {
+		if (err) throw err;
+
+		if (data !== null) {
+			return res.json(JSON.parse(data));
+		} else {
+			next();
+		}
+	});
+};
+
+const cacheSeries = (req, res, next) => {
+	client.get(req.params.id, (err, data) => {
+		if (err) throw err;
+
+		if (data !== null) {
+			return res.json(JSON.parse(data));
+		} else {
+			next();
+		}
+	});
+};
+
+router.get("/characters/:id", cacheCharacters, (req, res) => {
 	const baseUrl = "https://gateway.marvel.com:443/v1/public/characters/";
 	const url =
 		baseUrl +
@@ -26,11 +62,14 @@ router.get("/characters/:id", (req, res) => {
 
 	axios
 		.get(url)
-		.then(({ data }) => res.json(data.data))
+		.then(({ data }) => {
+			client.setex(req.params.id, 3600, JSON.stringify(data.data));
+			return res.json(data.data);
+		})
 		.catch(({ response }) => res.json(response.data));
 });
 
-router.get("/comics/:id", (req, res) => {
+router.get("/comics/:id", cacheComics, (req, res) => {
 	const baseUrl = "https://gateway.marvel.com:443/v1/public/comics/";
 	const url =
 		baseUrl +
@@ -46,11 +85,14 @@ router.get("/comics/:id", (req, res) => {
 
 	axios
 		.get(url)
-		.then(({ data }) => res.json(data.data))
+		.then(({ data }) => {
+			client.setex(req.params.id, 3600, JSON.stringify(data.data));
+			return res.json(data.data);
+		})
 		.catch(({ response }) => res.json(response.data));
 });
 
-router.get("/series/:id", (req, res) => {
+router.get("/series/:id", cacheSeries, (req, res) => {
 	const baseUrl = "https://gateway.marvel.com:443/v1/public/series/";
 	const url =
 		baseUrl +
@@ -66,7 +108,10 @@ router.get("/series/:id", (req, res) => {
 
 	axios
 		.get(url)
-		.then(({ data }) => res.json(data.data))
+		.then(({ data }) => {
+			client.setex(req.params.id, 3600, JSON.stringify(data.data));
+			return res.json(data.data);
+		})
 		.catch(({ response }) => res.json(response.data));
 });
 
